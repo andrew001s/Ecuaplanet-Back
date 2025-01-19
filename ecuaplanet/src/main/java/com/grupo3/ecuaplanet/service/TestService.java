e408fcef03775bbb143a065e0151368d3bdae412
andrew001s <andresroman45678@gmail.com>
2025-01-18 20:29:37 -0500

    feat: :sparkles: Implement TestController and TestService for testing purposes

---
+++ ecuaplanet/src/main/java/com/grupo3/ecuaplanet/service/TestService.java
@@ -0,0 +1,10 @@
+package com.grupo3.ecuaplanet.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TestService {
+    public String test() {
+        return "Hello from TestService";
+    }
+}
