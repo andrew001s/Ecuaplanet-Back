9c88e4bb22930410fc337e4a3426e137f60995a3
andrew001s <andresroman45678@gmail.com>
2025-01-18 19:31:16 -0500

    feat: :sparkles: Initialization of SpringBoot Project

---
+++ ecuaplanet/src/test/java/com/grupo3/ecuaplanet/EcuaplanetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo3.ecuaplanet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcuaplanetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
