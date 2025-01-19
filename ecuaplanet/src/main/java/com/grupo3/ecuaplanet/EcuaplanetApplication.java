34fbf259bd2309ce79d2a19ae80d90af35a4fd6c
andrew001s <andresroman45678@gmail.com>
2025-01-18 19:31:16 -0500

    feat: :sparkles: Initialization of SpringBoot Project

---
+++ ecuaplanet/src/main/java/com/grupo3/ecuaplanet/EcuaplanetApplication.java
@@ -0,0 +1,13 @@
+package com.grupo3.ecuaplanet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcuaplanetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcuaplanetApplication.class, args);
+	}
+
+}
