2a1dc486dc66a93fc7fac128ba1b8e3692168cf6
andrew001s <andresroman45678@gmail.com>
2025-01-26 12:32:37 -0500

    feat: :sparkles: Implementar cliente WebFlux y controlador para la API de Gemini

---
+++ ecuaplanet/src/main/java/com/grupo3/ecuaplanet/dto/GeminiResponseDto.java
@@ -0,0 +1,16 @@
+package com.grupo3.ecuaplanet.dto;
+
+import java.util.List;
+
+public class GeminiResponseDto {
+
+  private List<CandidateDto> candidates;
+
+  public List<CandidateDto> getCandidates() {
+    return candidates;
+  }
+
+  public void setCandidates(List<CandidateDto> candidates) {
+    this.candidates = candidates;
+  }
+}
