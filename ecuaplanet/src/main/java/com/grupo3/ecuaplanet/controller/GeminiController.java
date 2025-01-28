92175d5c94e506b0e88711dbf7a536d65cc875c7
isaaxc.bps <nauc8778@gmail.com>
2025-01-27 23:20:53 -0500

    feat: Agregar funcionalidad X al GeminiController

--- ecuaplanet/src/main/java/com/grupo3/ecuaplanet/controller/GeminiController.java
+++ ecuaplanet/src/main/java/com/grupo3/ecuaplanet/controller/GeminiController.java
@@ -1,25 +1,28 @@
 package com.grupo3.ecuaplanet.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.grupo3.ecuaplanet.dto.GeminiResponseDto;
 import com.grupo3.ecuaplanet.service.GeminiService;
-
-import reactor.core.publisher.Mono;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/api/gemini")
 public class GeminiController {
 
-    @Autowired
-    private GeminiService geminiService;
+    private final GeminiService geminiService;
+
+    // Inyección de dependencias por constructor (recomendado)
+    public GeminiController(GeminiService geminiService) {
+        this.geminiService = geminiService;
+    }
 
     @PostMapping("/response")
-    public Mono<GeminiResponseDto> getResponse(@RequestBody String text) {
+    public Mono<String> getResponse(@RequestBody String text) {
         return geminiService.getResponse(text);
     }
 
