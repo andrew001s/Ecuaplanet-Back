1333ebd6c0e9848e09019e40d0eca4f07debc5b0
isaaxc.bps <nauc8778@gmail.com>
2025-01-28 16:45:10 -0500

    refactor: Modificar en ProduccionBonchesRepository

--- ecuaplanet/src/main/java/com/grupo3/ecuaplanet/repository/ProduccionBonchesRepository.java
+++ ecuaplanet/src/main/java/com/grupo3/ecuaplanet/repository/ProduccionBonchesRepository.java
@@ -19,8 +19,7 @@ public interface ProduccionBonchesRepository extends JpaRepository<ProduccionBon
             "p.estado_bonche AS estadoBonche " +
             "FROM produccion_bonches p " +
             "JOIN productos_bonches pb ON pb.id_producto = p.id_producto " +
-            "JOIN variedades v ON v.id_variedad = pb.id_variedad", 
-            nativeQuery = true)
+            "JOIN variedades v ON v.id_variedad = pb.id_variedad", nativeQuery = true)
     List<ProduccionDto> obtenerProduccion();
 
 }
