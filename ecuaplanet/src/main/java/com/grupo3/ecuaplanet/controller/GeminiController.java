0622aba8782036f2c25dcdbf7bc6763bf91453ed
andrew001s <andresroman45678@gmail.com>
2025-01-28 23:05:08 -0500

    feat: :sparkles: Agregar soporte para CORS en GeminiController

--- ecuaplanet/src/main/java/com/grupo3/ecuaplanet/controller/GeminiController.java
+++ ecuaplanet/src/main/java/com/grupo3/ecuaplanet/controller/GeminiController.java
@@ -1,6 +1,7 @@
 package com.grupo3.ecuaplanet.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/api/gemini")
+@CrossOrigin(origins = "*")
 public class GeminiController {
 
     @Autowired
