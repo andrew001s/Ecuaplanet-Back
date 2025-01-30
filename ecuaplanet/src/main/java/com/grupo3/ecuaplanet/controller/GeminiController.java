842515a4a4a0eaa0ccaa1145439b3b2a343f0b24
andrew001s <andresroman45678@gmail.com>
2025-01-30 16:25:34 -0500

    feat: :sparkles: Agregar clases DTO para manejo de vectores y embeddings; implementar lógica de consulta en servicios de producción y cultivo

--- ecuaplanet/src/main/java/com/grupo3/ecuaplanet/controller/GeminiController.java
+++ ecuaplanet/src/main/java/com/grupo3/ecuaplanet/controller/GeminiController.java
@@ -1,5 +1,9 @@
 package com.grupo3.ecuaplanet.controller;
 
+
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,9 +11,15 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.grupo3.ecuaplanet.constants.GeminiConstants;
+import com.grupo3.ecuaplanet.dto.CultivoDto;
+import com.grupo3.ecuaplanet.dto.EmbeddingDto;
 import com.grupo3.ecuaplanet.dto.GeminiResponseDto;
+import com.grupo3.ecuaplanet.dto.ProduccionDto;
 import com.grupo3.ecuaplanet.dto.RequestGeminiDto;
+import com.grupo3.ecuaplanet.service.CultivoService;
 import com.grupo3.ecuaplanet.service.GeminiService;
+import com.grupo3.ecuaplanet.service.ProduccionService;
 
 import reactor.core.publisher.Mono;
 
@@ -20,26 +30,31 @@ public class GeminiController {
 
     @Autowired
     private GeminiService geminiService;
-    
-    private final static String PROMP_STRING = "Eres un asistente que ayuda a los usuarios a obtener información de una base de datos de una floricola. \n" + //
-                "Responde las preguntas del usuario basándote *únicamente* en la siguiente información extraída de la base de datos.\n" + //
-                "Si no puedes responder la pregunta con la información dada, responde: \"No tengo información suficiente para responder esa pregunta\". \n" + //
-                "No inventes información. \n" ;
-    
+
+    @Autowired
+    private CultivoService cultivoService;
+
+    @Autowired
+    private ProduccionService produccionService;
+
     @PostMapping("/cultivo")
     public Mono<GeminiResponseDto> getCultivo(@RequestBody RequestGeminiDto requestGeminiDto) {
-
-        String response = PROMP_STRING + requestGeminiDto.getText() + " \n";
+        String response = requestGeminiDto.getText();
+        EmbeddingDto embeddingDto = geminiService.getEmbedding(response);
+        String arrays = Arrays.toString(embeddingDto.getEmbedding().getValues());
+        List<CultivoDto> cultivo = cultivoService.obtenercultivo(arrays);
+        response += GeminiConstants.PROMP_STRING + cultivoService.listToString(cultivo);
         return geminiService.getCultivo(response);
     }
 
     @PostMapping("/produccion")
     public Mono<GeminiResponseDto> getProduccion(@RequestBody RequestGeminiDto requestGeminiDto) {
-        
-        String response = PROMP_STRING + requestGeminiDto.getText() + " \n";
+        String response = requestGeminiDto.getText();
+        EmbeddingDto embeddingDto = geminiService.getEmbedding(response);
+        String arrays = Arrays.toString(embeddingDto.getEmbedding().getValues());
+        List<ProduccionDto> produccion = produccionService.obtenerProduccion(arrays); 
+        response += GeminiConstants.PROMP_STRING + produccionService.listToString(produccion);
         return geminiService.getProduccion(response);
 
     }
-    
-
 }
--- ecuaplanet/src/main/java/com/grupo3/ecuaplanet/model/IngresoMallas.java
+++ ecuaplanet/src/main/java/com/grupo3/ecuaplanet/model/IngresoMallas.java
@@ -1,5 +1,8 @@
 package com.grupo3.ecuaplanet.model;
 
+import java.sql.Timestamp;
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -25,6 +28,31 @@ public class IngresoMallas {
     @Column(name = "cantidad_mallas_ingreso")
     private int cantidadMallas;
 
+    @Column(name = "tallos_por_malla")
+    private int tallosPorMalla;
+
+    @Column(name = "fecha_hora_ingreso")
+    private Timestamp fechaHoraIngreso;
+
+    @Column(name="embedding",columnDefinition = "vector")
+    private List<Float> embedding;
+
+    public List<Float> getEmbedding() {
+        return embedding;
+    }
+
+    public void setEmbedding(List<Float> embedding) {
+        this.embedding = embedding;
+    }
+
+    public Timestamp getFechaHoraIngreso() {
+        return fechaHoraIngreso;
+    }
+
+    public void setFechaHoraIngreso(Timestamp fechaHoraIngreso) {
+        this.fechaHoraIngreso = fechaHoraIngreso;
+    }
+
     public int getIdIngresoMallas() {
         return idIngresoMallas;
     }
@@ -49,7 +77,13 @@ public class IngresoMallas {
     public void setCantidadMallas(int cantidadMallas) {
         this.cantidadMallas = cantidadMallas;
     }
+
+    public int getTallosPorMalla() {
+        return tallosPorMalla;
+    }
+
+    public void setTallosPorMalla(int tallosPorMalla) {
+        this.tallosPorMalla = tallosPorMalla;
+    }
     
-
-
 }
