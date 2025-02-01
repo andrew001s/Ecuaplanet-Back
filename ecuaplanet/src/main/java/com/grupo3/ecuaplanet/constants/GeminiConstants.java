882fedcda5a0c36fa881bb596798ff3ec2bbf35a
andrew001s <andresroman45678@gmail.com>
2025-02-01 16:18:28 -0500

    feat: :sparkles: Agregar QdrantService para búsqueda de vectores, Agregar logica para Clasificacion de Peticiones como General o Especifica

--- ecuaplanet/src/main/java/com/grupo3/ecuaplanet/constants/GeminiConstants.java
+++ ecuaplanet/src/main/java/com/grupo3/ecuaplanet/constants/GeminiConstants.java
@@ -12,4 +12,28 @@ public interface GeminiConstants {
             + " información suficiente para responder esa pregunta\". \n"
             + //
             "No inventes información. \n";
+
+    static final String PROMP_STRING_CLASSIFIER = "Eres un asistente que clasifica preguntas como General o Específicas."
+            + " Solo responde con una de las dos opciones: General o Específica."
+            + " Sigue estas instrucciones para clasificar correctamente:\n\n"
+            + "1. **Clasifica como 'Específica' si la pregunta incluye alguno de estos elementos:**\n"
+            + "   - **Nombres de variedades específicas:** cualquier mención de una variedad de rosa como 'Freedom', 'Montial', 'Vendela', etc.\n"
+            + "   - **Fechas específicas:** como 'enero', 'marzo', '2024', 'primer trimestre del año'.\n"
+            + "   - **Nombres de clientes específicos:** por ejemplo, 'Cliente XYZ'.\n"
+            + "   - **Referencias cuantitativas específicas:** número de tallos o cantidades detalladas.\n\n"
+            + "2. **Clasifica como 'General' si la pregunta no contiene ninguno de los elementos mencionados:**\n"
+            + "   - Las preguntas generales suelen pedir información amplia o tendencias generales sin detalles específicos.\n\n"
+            + "### Ejemplos para clasificar preguntas:\n"
+            + "1. ¿Cuántos tallos tienen Freedom? : Específica \n"
+            + "2. ¿Cuál es la variedad de rosas más vendida? : General \n"
+            + "3. ¿Cuál es el precio de las rosas? : General \n"
+            + "4. ¿Cuál es el precio de las rosas Freedom? : Específica \n"
+            + "5. ¿Cuál es el precio de las rosas Freedom en el mes de enero? : Específica \n"
+            + "6. Dame un resumen de cultivos : General \n"
+            + "7. Genera un resumen de Freedom : Específica \n"
+            + "8. ¿Cuál es el cliente que más compra? : General \n"
+            + "9. ¿Cuál es el cliente que más compra en el mes de enero? : General \n"
+            + "10. ¿Cuál es el cliente que más compra en enero de rosas Freedom? : Específica \n\n"
+            + "Recuerda: cualquier mención de un **nombre de variedad (como Freedom)** automáticamente convierte la pregunta en **Específica.**";
+            
 }
--- ecuaplanet/src/main/java/com/grupo3/ecuaplanet/service/GeminiService.java
+++ ecuaplanet/src/main/java/com/grupo3/ecuaplanet/service/GeminiService.java
@@ -8,11 +8,14 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import com.grupo3.ecuaplanet.constants.GeminiConstants;
 import com.grupo3.ecuaplanet.dto.BodyGeminiDto;
+import com.grupo3.ecuaplanet.dto.CandidateDto;
 import com.grupo3.ecuaplanet.dto.ContentDto;
 import com.grupo3.ecuaplanet.dto.EmbeddingDto;
 import com.grupo3.ecuaplanet.dto.GeminiResponseDto;
 import com.grupo3.ecuaplanet.dto.PartDto;
+import com.grupo3.ecuaplanet.dto.RequestGeminiDto;
 
 import reactor.core.publisher.Mono;
 
@@ -22,28 +25,42 @@ public class GeminiService {
     @Value("${gemini.api.key}")
     private String ApiKey;
 
-
     @Autowired
     private WebClient.Builder webClientBuilder;
 
     public Mono<GeminiResponseDto> getRequest(String text) {
         return makeGeminiRequest(text);
     }
-    
+
+    private Mono<GeminiResponseDto> sendGeminiRequest(BodyGeminiDto body) {
+        return webClientBuilder
+                .build()
+                .post()
+                .uri("https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key="
+                        + ApiKey)
+                .bodyValue(body)
+                .retrieve()
+                .bodyToMono(GeminiResponseDto.class);
+
+    }
+
     @Async
     private Mono<GeminiResponseDto> makeGeminiRequest(String text) {
-        
+
         PartDto part = new PartDto(text);
         ContentDto contents = new ContentDto(List.of(part));
         BodyGeminiDto body = new BodyGeminiDto(contents);
 
-        return webClientBuilder
-                .build()
-                .post()
-                .uri("https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=" + ApiKey)
-                .bodyValue(body)
-                .retrieve()
-                .bodyToMono(GeminiResponseDto.class);
+        return sendGeminiRequest(body);
+    }
+
+    @Async
+    public Mono<GeminiResponseDto> classifierGeminiRequest(String text) {
+
+        PartDto part = new PartDto(text);
+        ContentDto contents = new ContentDto(List.of(part));
+        BodyGeminiDto body = new BodyGeminiDto(contents);
+        return sendGeminiRequest(body);
     }
 
     @Async
@@ -51,13 +68,23 @@ public class GeminiService {
         return webClientBuilder
                 .build()
                 .post()
-                .uri("https://generativelanguage.googleapis.com/v1beta/models/text-embedding-004:embedContent?key=" + ApiKey)
+                .uri("https://generativelanguage.googleapis.com/v1beta/models/text-embedding-004:embedContent?key="
+                        + ApiKey)
                 .header("Content-Type", "application/json")
-                .bodyValue("{\"model\": \"models/text-embedding-004\", \"content\": {\"parts\": [{\"text\": \"" + text + "\"}]}}")
+                .bodyValue("{\"model\": \"models/text-embedding-004\", \"content\": {\"parts\": [{\"text\": \"" + text
+                        + "\"}]}}")
                 .retrieve()
                 .bodyToMono(EmbeddingDto.class)
                 .block();
     }
-    
-    
+
+    public String clasificador(RequestGeminiDto RequestGeminiDto) {
+        String response = GeminiConstants.PROMP_STRING_CLASSIFIER + RequestGeminiDto.getText();
+        GeminiResponseDto geminiResponse = classifierGeminiRequest(response).block();
+        CandidateDto candidate = geminiResponse.getCandidates().get(0);
+        String valueClasificate = candidate.getContent().getParts().get(0).getText();
+        valueClasificate = valueClasificate.replaceAll("\\s+", "");
+        return valueClasificate;
+    }
+
 }
